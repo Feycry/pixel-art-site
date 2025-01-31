be61d4349e784f9694426f54f603893f9001c7ad
Basti <sebastian.olander@aalto.fi>
2025-02-01 01:27:08 +0200

    index.html template

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello world!"
+    words = ["apina", "banaani", "cembalo"]
+    return render_template("index.html", message="Tervetuloa!", items=words)
