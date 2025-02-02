19cbb19f66b155286f3330c0f055d16f757e0187
Basti <sebastian.olander@aalto.fi>
2025-02-02 13:12:34 +0200

    Using keskustelu_v1 as template

--- app.py
+++ app.py
@@ -1,52 +1,105 @@
+import sqlite3
 from flask import Flask
 from flask import redirect, render_template, request, session
-from werkzeug.security import check_password_hash
-import config
-import db
+import config, forum, users
 
 app = Flask(__name__)
 app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    threads = forum.get_threads()
+    print(threads)
+    return render_template("index.html", threads=threads)
 
-@app.route("/login", methods=["POST"])
-def login():
-    username = request.form["username"]
-    password = request.form["password"]
-    
-    sql = "SELECT password_hash FROM users WHERE username = ?"
-    password_hash = db.query(sql, [username])[0][0]
+@app.route("/thread/<int:thread_id>")
+def show_thread(thread_id):
+    thread = forum.get_thread(thread_id)
+    messages = forum.get_messages(thread_id)
+    return render_template("thread.html", thread=thread, messages=messages)
 
-    if check_password_hash(password_hash, password):
-        session["username"] = username
-        return redirect("/")
-    else:
-        return "VIRHE: väärä tunnus tai salasana"
+@app.route("/new_thread", methods=["POST"])
+def new_thread():
+    title = request.form["title"]
+    content = request.form["content"]
+    user_id = session["user_id"]
 
-@app.route("/logout")
-def logout():
-    del session["username"]
-    return redirect("/")
+    thread_id = forum.add_thread(title, content, user_id)
+    return redirect("/thread/" + str(thread_id))
+
+@app.route("/new_message", methods=["POST"])
+def new_message():
+    content = request.form["content"]
+    user_id = session["user_id"]
+    thread_id = request.form["thread_id"]
+
+    forum.add_message(content, user_id, thread_id)
+    return redirect("/thread/" + str(thread_id))
+
+@app.route("/edit/<int:message_id>", methods=["GET", "POST"])
+def edit_message(message_id):
+    message = forum.get_message(message_id)
+
+    if request.method == "GET":
+        return render_template("edit.html", message=message)
+
+    if request.method == "POST":
+        content = request.form["content"]
+        forum.update_message(message["id"], content)
+        return redirect("/thread/" + str(message["thread_id"]))
+
+@app.route("/remove/<int:message_id>", methods=["GET", "POST"])
+def remove_message(message_id):
+    message = forum.get_message(message_id)
+
+    if request.method == "GET":
+        return render_template("remove.html", message=message)
+
+    if request.method == "POST":
+        if "continue" in request.form:
+            forum.remove_message(message["id"])
+        return redirect("/thread/" + str(message["thread_id"]))
 
 @app.route("/register")
 def register():
     return render_template("register.html")
 
-@app.route("/create", methods=["POST"])
-def create():
-    username = request.form["username"]
-    password1 = request.form["password1"]
-    password2 = request.form["password2"]
-    if password1 != password2:
-        return "VIRHE: salasanat eivät ole samat"
-    password_hash = generate_password_hash(password1)
+@app.route("/new_user", methods=["GET", "POST"])
+def new_user():
+    if request.method == "GET":
+        return render_template("register.html")
 
-    try:
-        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
-        db.execute(sql, [username, password_hash])
-    except sqlite3.IntegrityError:
-        return "VIRHE: tunnus on jo varattu"
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
 
-    return "Tunnus luotu"
+        if password1 != password2:
+            return "VIRHE: salasanat eivät ole samat"
+
+        try:
+            users.create_user(username, password1)
+            return "Tunnus luotu"
+        except sqlite3.IntegrityError:
+            return "VIRHE: tunnus on jo varattu"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user_id = users.check_login(username, password)
+        if user_id:
+            session["user_id"] = user_id
+            return redirect("/")
+        else:
+            return "VIRHE: väärä tunnus tai salasana"
+
+@app.route("/logout")
+def logout():
+    del session["user_id"]
+    return redirect("/")
